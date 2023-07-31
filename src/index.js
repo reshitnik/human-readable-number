fa55fa6fed00397d7af4d13d8e81cf9464499a69
reshitnik <reshitnik@gmail.com>
2023-07-31 20:01:15 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    const units = ["", "one","two", "three", "four", "five", "six", "seven", "eight","nine","ten","eleven","twelve" ,"thirteen","fourteen","fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["twenty","thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const hundred = ["", "hundred", "hundred", "hundred", "hundred","hundred", "hundred","hundred","hundred","hundred"];
+    if (number=== 0) {
+        return "zero";
+    } else if (number % 100 < 20){
+        return `${units[Math.floor(number/100)]} ${hundred[Math.floor(number/100)]} ${units[number % 100]}`.trim();
+    } else if (number % 100 > 19) {
+        return `${units[Math.floor(number/100)]} ${hundred[Math.floor(number/100)]} ${tens[((number%100)-(number%10))/10-2]} ${units[(number%10)]}`.trim()};
+
 }
+
+
